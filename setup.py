cef3fb867bc67ef02cea2e11fcccd75208f10bd3
Tarek Ziade <tarek@ziade.org>
2016-02-17 10:39:43 +0000

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+import codecs
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_file(filename):
+    """Open a related file and return its content."""
+    with codecs.open(os.path.join(here, filename), encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+README = read_file('README.rst')
+REQUIREMENTS = ["kinto", "kinto-attachment", "kinto-changes"]
+ENTRY_POINTS = {}
+DEPENDENCY_LINKS = []
+
+setup(name='kinto-dist',
+      version='0.1.0',
+      description='Kinto Distribution',
+      long_description=README,
+      license='Apache License (2.0)',
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Programming Language :: Python :: Implementation :: PyPy",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+          "License :: OSI Approved :: Apache Software License"
+      ],
+      keywords="web services",
+      author='Mozilla Services',
+      author_email='services-dev@mozilla.com',
+      url='https://github.com/Kinto/kinto-dist',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=REQUIREMENTS)
