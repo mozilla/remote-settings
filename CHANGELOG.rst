35fb783f90c11638cbf5761650e75ecca726e72a
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-03-11 13:36:56 +0100

    Back to development: 0.2.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -3,6 +3,12 @@ CHANGELOG
 
 This document describes changes between each past release.
 
+0.2.0 (unreleased)
+==================
+
+- Nothing changed yet.
+
+
 0.1.0 (2016-03-11)
 ==================
 
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ ENTRY_POINTS = {}
 DEPENDENCY_LINKS = []
 
 setup(name='kinto-dist',
-      version='0.1.0',
+      version='0.2.0.dev0',
       description='Kinto Distribution',
       long_description=README + "\n\n" + CHANGELOG,
       license='Apache License (2.0)',
