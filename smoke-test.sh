4f0fc73749899e324cc2388a47c9b5bb989324bf
Mathieu Leplatre <mathieu@mozilla.com>
2016-07-15 08:08:09 +0200

    Do not use default bucket for attachments test

--- smoke-test.sh
+++ smoke-test.sh
@@ -4,15 +4,16 @@ set -e
 # Show executed commands
 set -x
 
-# kinto-attachment test
-curl --fail "http://localhost:8888/v1/__heartbeat__"
-curl --fail -X POST -H "Content-Type: multipart/form-data" -H "Authorization: Basic dXNlcjpwYXNz" -F "attachment=@/etc/hostname" "http://localhost:8888/v1/buckets/default/collections/articles/records/abcde/attachment"
-# kinto-signer test
 curl --fail -X PUT -H "Authorization: Basic dXNlcjpwYXNz" "http://localhost:8888/v1/buckets/source"
 curl --fail -X PUT -H "Authorization: Basic dXNlcjpwYXNz" "http://localhost:8888/v1/buckets/source/collections/source"
+
+# kinto-attachment test
+curl --fail "http://localhost:8888/v1/__heartbeat__"
+curl --fail -X POST -H "Content-Type: multipart/form-data" -H "Authorization: Basic dXNlcjpwYXNz" -F "attachment=@/etc/hostname" "http://localhost:8888/v1/buckets/source/collections/source/records/abcde/attachment"
+# kinto-signer test
 curl --fail -X PUT -H "Authorization: Basic dXNlcjpwYXNz" "http://localhost:8888/v1/buckets/source/collections/source/records/xxyz"
 curl --fail -X PATCH -H "Authorization: Basic dXNlcjpwYXNz"  --header "Content-Type:application/json"  --data '{"data": {"status":"to-sign"}}' "http://localhost:8888/v1/buckets/source/collections/source"
-curl "http://localhost:8888/v1/buckets/destination/collections/destination" | grep '"signed"'
+curl "http://localhost:8888/v1/buckets/destination/collections/destination" | grep '"signature"'
 curl "http://localhost:8888/v1/buckets/destination/collections/destination/records" | grep '"xxyz"'
 # kinto-changes
 curl "http://localhost:8888/v1/buckets/monitor/collections/changes/records" | grep '"source"'
