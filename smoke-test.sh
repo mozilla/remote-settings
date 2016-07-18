1fd7d7b218b4e69681dbaf874f01d0ca5ff8d472
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-07-18 16:03:52 +0200

    @leplatrem review.

--- smoke-test.sh
+++ smoke-test.sh
@@ -5,21 +5,21 @@ set -e -x
 source .venv/bin/activate
 pip install httpie
 
-http PUT http://localhost:8888/v1/buckets/source Authorization:"Basic dXNlcjpwYXNz"
-http PUT http://localhost:8888/v1/buckets/source/collections/source Authorization:"Basic dXNlcjpwYXNz"
+http --check-status PUT http://localhost:8888/v1/buckets/source Authorization:"Basic dXNlcjpwYXNz"
+http --check-status PUT http://localhost:8888/v1/buckets/source/collections/source Authorization:"Basic dXNlcjpwYXNz"
 
-http "http://localhost:8888/v1/__heartbeat__"
+http --check-status "http://localhost:8888/v1/__heartbeat__"
 
 # kinto-attachment test
 curl -O "http://kinto.readthedocs.io/en/stable/_images/kinto-logo.png"
 
-http POST http://localhost:8888/v1/buckets/source/collections/source/records/abcde/attachment Authorization:"Basic dXNlcjpwYXNz" Content-Type:"multipart/form-data" attachment=@kinto-logo.png
+http --check-status --form POST http://localhost:8888/v1/buckets/source/collections/source/records/80ec9929-6896-4022-8443-3da4f5353f47/attachment Authorization:"Basic dXNlcjpwYXNz" attachment@kinto-logo.png
 
 # kinto-signer test
-http PUT http://localhost:8888/v1/buckets/source/collections/source/records/xxyz Authorization:"Basic dXNlcjpwYXNz"
-echo '{"data": {"status":"to-sign"}}' | http PATCH http://localhost:8888/v1/buckets/source/collections/source Authorization:"Basic dXNlcjpwYXNz"
-http http://localhost:8888/v1/buckets/destination/collections/destination | grep '"signature"'
-http http://localhost:8888/v1/buckets/destination/collections/destination/records | grep '"xxyz"'
+http --check-status PUT http://localhost:8888/v1/buckets/source/collections/source/records/xxyz Authorization:"Basic dXNlcjpwYXNz"
+echo '{"data": {"status":"to-sign"}}' | http --check-status PATCH http://localhost:8888/v1/buckets/source/collections/source Authorization:"Basic dXNlcjpwYXNz"
+http --check-status http://localhost:8888/v1/buckets/destination/collections/destination | grep '"signature"'
+http --check-status http://localhost:8888/v1/buckets/destination/collections/destination/records | grep '"xxyz"'
 
 # kinto-changes
-http http://localhost:8888/v1/buckets/monitor/collections/changes/records | grep '"source"'
+http --check-status http://localhost:8888/v1/buckets/monitor/collections/changes/records | grep '"source"'
