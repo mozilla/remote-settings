304999b55bb43c6ac17a7d5d9502441878b9fca0
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-07-18 15:32:24 +0200

    Try to debug with httpie.

--- smoke-test.sh
+++ smoke-test.sh
@@ -2,21 +2,24 @@
 # Show executed commands
 set -e -x
 
-curl --fail -v -X PUT -H "Authorization: Basic dXNlcjpwYXNz" "http://localhost:8888/v1/buckets/source"
-curl --fail -v -X PUT -H "Authorization: Basic dXNlcjpwYXNz" "http://localhost:8888/v1/buckets/source/collections/source"
+source .venv/bin/activate
+pip install httpie
 
-curl --fail -v "http://localhost:8888/v1/__heartbeat__"
+http PUT http://localhost:8888/v1/buckets/source Authorization:"Basic dXNlcjpwYXNz"
+http PUT http://localhost:8888/v1/buckets/source/collections/source Authorization:"Basic dXNlcjpwYXNz"
+
+http "http://localhost:8888/v1/__heartbeat__"
 
 # kinto-attachment test
 curl -O "http://kinto.readthedocs.io/en/stable/_images/kinto-logo.png"
-sleep 1
-curl --fail -v -X POST -H "Content-Type: multipart/form-data" -H "Authorization: Basic dXNlcjpwYXNz" -F "attachment=@kinto-logo.png" "http://localhost:8888/v1/buckets/source/collections/source/records/abcde/attachment"
+
+http POST http://localhost:8888/v1/buckets/source/collections/source/records/abcde/attachment Authorization:"Basic dXNlcjpwYXNz" Content-Type:"multipart/form-data" attachment=@kinto-logo.png
 
 # kinto-signer test
-curl --fail -v -X PUT -H "Authorization: Basic dXNlcjpwYXNz" "http://localhost:8888/v1/buckets/source/collections/source/records/xxyz"
-curl --fail -v -X PATCH -H "Authorization: Basic dXNlcjpwYXNz"  --header "Content-Type:application/json"  --data '{"data": {"status":"to-sign"}}' "http://localhost:8888/v1/buckets/source/collections/source"
-curl "http://localhost:8888/v1/buckets/destination/collections/destination" | grep '"signature"'
-curl "http://localhost:8888/v1/buckets/destination/collections/destination/records" | grep '"xxyz"'
+http PUT http://localhost:8888/v1/buckets/source/collections/source/records/xxyz Authorization:"Basic dXNlcjpwYXNz"
+echo '{"data": {"status":"to-sign"}}' | http PATCH http://localhost:8888/v1/buckets/source/collections/source Authorization:"Basic dXNlcjpwYXNz"
+http http://localhost:8888/v1/buckets/destination/collections/destination | grep '"signature"'
+http http://localhost:8888/v1/buckets/destination/collections/destination/records | grep '"xxyz"'
 
 # kinto-changes
-curl "http://localhost:8888/v1/buckets/monitor/collections/changes/records" | grep '"source"'
+http http://localhost:8888/v1/buckets/monitor/collections/changes/records | grep '"source"'
