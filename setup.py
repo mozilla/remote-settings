d13a7028b5dae26869fe7383b1c72101427ddbba
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-08-29 10:59:09 +0200

    Upgrade to Kinto 4.1.1 and Kinto-Signer 0.8.1

--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@ README = read_file('README.rst')
 CHANGELOG = read_file('CHANGELOG.rst')
 
 REQUIREMENTS = [
-    "kinto[postgresql,monitoring]>=4.0,<4.1",
+    "kinto[postgresql,monitoring]>=4.1,<4.2",
     "kinto-attachment>=0.8.0,<0.9",
     "kinto-amo>=0.2.0,<0.3",
     "kinto-changes>=0.3,<0.4",
-    "kinto-signer>=0.7.3,<0.8",
-    "kinto-fxa>=2.0.0,<3.0",
+    "kinto-signer>=0.8.1,<0.9",
+    "kinto-fxa>=2.0,<3.0",
     "kinto-ldap>=0.1,<0.2",
     "boto>=2.40,<2.41",
 ]
