3e99816afd793b52edfdad658fa3ebb4214a5ae3
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-09-08 18:09:25 +0200

    Pin amo2kinto version too.
    @glasserc review.

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ README = read_file('README.rst')
 CHANGELOG = read_file('CHANGELOG.rst')
 
 REQUIREMENTS = [
+    "amo2kinto>=1.5.1,<1.6",
     "kinto[postgresql,monitoring]>=4.1,<4.2",
     "kinto-attachment>=1.0,<1.1",
     "kinto-amo>=0.2.0,<0.3",
