febc25a1f2fb4ddb79b52c07469b81dd03dfc5a2
Mathieu Leplatre <mathieu@mozilla.com>
2016-09-30 12:41:10 +0200

    Use ENV variables with default values for smoke test

--- smoke-test.sh
+++ smoke-test.sh
@@ -5,16 +5,17 @@ set -e -x
 source .venv/bin/activate
 pip install httpie kinto-http
 
-SERVER=http://localhost:8888/v1
+SERVER="${SERVER:-http://localhost:8888/v1}"
+AUTH="${AUTH:-user:pass}"
 
-http --check-status PUT $SERVER/buckets/source --auth user:pass
-http --check-status PUT $SERVER/buckets/source/collections/source --auth user:pass
+http --check-status PUT $SERVER/buckets/source --auth $AUTH
+http --check-status PUT $SERVER/buckets/source/collections/source --auth $AUTH
 
 http --check-status $SERVER/__heartbeat__
 
 # kinto-attachment test
 curl -O "http://kinto.readthedocs.io/en/stable/_images/kinto-logo.png"
-http --check-status --form POST $SERVER/buckets/source/collections/source/records/80ec9929-6896-4022-8443-3da4f5353f47/attachment attachment@kinto-logo.png --auth user:pass
+http --check-status --form POST $SERVER/buckets/source/collections/source/records/80ec9929-6896-4022-8443-3da4f5353f47/attachment attachment@kinto-logo.png --auth $AUTH
 
 # kinto-signer test
 curl -O https://raw.githubusercontent.com/Kinto/kinto-signer/0.8.1/scripts/e2e.py
