faa766445cd5a5c9dd263e9043443672838c8fb0
Mathieu Leplatre <mathieu@mozilla.com>
2016-11-15 09:28:55 +0100

    Preparing release 1.2

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -4,7 +4,7 @@ CHANGELOG
 This document describes changes between each past release as well as
 the version control of each dependency.
 
-1.2 (unreleased)
+1.2 (2016-11-15)
 ================
 
 Kinto
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ ENTRY_POINTS = {}
 DEPENDENCY_LINKS = []
 
 setup(name='kinto-dist',
-      version='1.2.dev0',
+      version='1.2',
       description='Kinto Distribution',
       long_description=README + "\n\n" + CHANGELOG,
       license='Apache License (2.0)',
