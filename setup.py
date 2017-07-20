9669f06378ce3fba86f72d0d4beb976655992029
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-07-21 00:35:56 +0200

    Preparing release 3.2.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ REQUIREMENTS = [
     "kinto-amo>=0.4.0,<0.5",
     "kinto-changes>=1.0.0,<1.1.0",
     "kinto-emailer>=1.0,<1.1",
-    "kinto-signer>=1.5.3,<1.6",
+    "kinto-signer>=1.5.4,<1.6",
     "kinto-fxa>=2.3,<3.0",
     "kinto-ldap>=0.3.0,<0.4",
     "amo2kinto>=2.0,<2.1",
@@ -32,7 +32,7 @@ ENTRY_POINTS = {}
 DEPENDENCY_LINKS = []
 
 setup(name='kinto-dist',
-      version='3.3.0.dev0',
+      version='3.2.3',
       description='Kinto Distribution',
       long_description=README + "\n\n" + CHANGELOG,
       license='Apache License (2.0)',
