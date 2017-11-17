6ace1069fa969781de24011541c75d75045cbd47
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-11-17 19:13:52 +0100

    Back to development: 4.6.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ ENTRY_POINTS = {}
 DEPENDENCY_LINKS = []
 
 setup(name='kinto-dist',
-      version='4.5.0',
+      version='4.6.0.dev0',
       description='Kinto Distribution',
       long_description=README + "\n\n" + CHANGELOG,
       license='Apache License (2.0)',
