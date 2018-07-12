a5ed55c2de4b9842616f6bdc11e157888f971238
Peter Bengtsson <mail@peterbe.com>
2018-07-12 16:52:30 -0400

    solution to dogstatsd_plugin.so

--- bin/docker-install.sh
+++ bin/docker-install.sh
@@ -20,6 +20,9 @@ apt-get install -y --no-install-recommends \
 # Python packages
 pip install --no-cache-dir -r default.txt -r prod.txt -c constraints.txt
 uwsgi --build-plugin https://github.com/Datadog/uwsgi-dogstatsd
+# The above command puts the `dogstatsd_plugin.so` in the root.
+# Move it so it becomes available in the WORKDIR.
+mv /dogstatsd_plugin.so /app/
 
 
 # Cleanup
