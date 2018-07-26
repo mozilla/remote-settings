f06acc083f9d1420923cab2fc2bbb3d1ab3605ad
Mathieu Leplatre <mathieu@mozilla.com>
2018-07-26 12:44:53 +0200

    Prepare version 8.0.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ CHANGELOG = read_file('CHANGELOG.rst')
 
 setup(
     name='kinto-dist',
-    version='8.0.1',
+    version='8.0.2',
     description='Kinto Distribution',
     long_description=README + "\n\n" + CHANGELOG,
     license='Apache License (2.0)',
