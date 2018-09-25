f64cb2546b9ef1d757c3c5ec142c293163f4a710
Ethan Glasser-Camp <ethan@betacantrips.com>
2018-09-25 14:26:45 -0400

    Fix typo

--- tests/run.sh
+++ tests/run.sh
@@ -17,7 +17,7 @@ usage() {
 
 case $1 in
   start)
-    wget -q --tries=10 --retry-connrefused --waitretry=1 -O /dev/null $SERVER || (echo "Can't reach $SERRVER" && exit 1)
+    wget -q --tries=10 --retry-connrefused --waitretry=1 -O /dev/null $SERVER || (echo "Can't reach $SERVER" && exit 1)
     http --check-status $SERVER/__heartbeat__
     # http --check-status $SERVER/__api__
     http POST "$SERVER/__flush__"
