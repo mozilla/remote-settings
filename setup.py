7b7263a5f4741961997c1c2e34adcaf9d4c6c7e3
Mathieu Leplatre <mathieu@mozilla.com>
2018-10-22 23:26:45 +0200

    Preparing release 11.0.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ CHANGELOG = read_file('CHANGELOG.rst')
 
 setup(
     name='kinto-dist',
-    version='10.0.3.dev0',
+    version='11.0.0',
     description='Kinto Distribution',
     long_description=README + "\n\n" + CHANGELOG,
     license='Apache License (2.0)',
