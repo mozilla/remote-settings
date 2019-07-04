1688956c846e7760757c638ed800a118d5da6910
Mathieu Leplatre <mathieu@mozilla.com>
2019-07-04 16:36:45 +0200

    Back to development: 17.1.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ CHANGELOG = read_file('CHANGELOG.rst')
 
 setup(
     name='kinto-dist',
-    version='17.1.3',
+    version='17.1.4.dev0',
     description='Kinto Distribution',
     long_description=README + "\n\n" + CHANGELOG,
     license='Apache License (2.0)',
