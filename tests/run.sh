8a2c7eb127a75a7dfcb6cd4c1b494469dd1fe17c
Mathieu Leplatre <mathieu@mozilla.com>
2019-10-17 12:38:49 +0200

    Wait longer for memcached

--- tests/run.sh
+++ tests/run.sh
@@ -16,7 +16,7 @@ usage() {
 
 case $1 in
   start)
-    wget -q --tries=10 --retry-connrefused --waitretry=1 -O /dev/null $SERVER || (echo "Can't reach $SERVER" && exit 1)
+    wget -q --tries=180 --retry-connrefused --waitretry=1 -O /dev/null $SERVER || (echo "Can't reach $SERVER" && exit 1)
     http --check-status $SERVER/__heartbeat__
     http POST "$SERVER/__flush__"
     SERVER=$SERVER ./smoke-test.sh
