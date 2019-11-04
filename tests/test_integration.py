421b03c6c3e09ba41acb09037806f23170d94dd0
jrbenny35 <bennyjr169@gmail.com>
2019-11-04 11:21:49 -0600

    Add small integration test suite.

---
+++ tests/test_integration.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture
+def firefox_options(firefox_options):
+    """Set Firefox Options."""
+    firefox_options.headless = True
+    return firefox_options
+
+@pytest.mark.nondestructive
+def test_html_loads_correctly(base_url, selenium):
+    selenium.get(base_url)
+    header = selenium.find_element_by_css_selector(".content div > h1")
+    assert "Kinto Administration" in header.text
