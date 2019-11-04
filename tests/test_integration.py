7d2c23c2fa65e9a76dc8f55d8837c64310709e10
jrbenny35 <bennyjr169@gmail.com>
2019-11-04 11:25:41 -0600

    Update to test.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -12,3 +12,4 @@ def test_html_loads_correctly(base_url, selenium):
     selenium.get(base_url)
     header = selenium.find_element_by_css_selector(".content div > h1")
     assert "Kinto Administration" in header.text
+    assert header.is_displayed()
