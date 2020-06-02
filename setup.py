07e1235f8b32f12694aae954b4a3fd7681c8c4af
Mathieu Leplatre <mathieu@mozilla.com>
2020-06-02 19:51:00 +0200

    Preparing release 19.0.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name='kinto-dist',
-    version='19.0.2.dev0',
+    version='19.0.2',
     description='Kinto Distribution',
     long_description=README + "\n\n" + CHANGELOG,
     license='Apache License (2.0)',
