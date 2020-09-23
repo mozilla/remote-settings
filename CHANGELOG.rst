f4181a256d08036f3ad374005310e2f433d56088
Mathieu Leplatre <mathieu@mozilla.com>
2020-09-23 09:38:35 +0200

    Preparing release 21.1.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -5,7 +5,7 @@ This document describes changes between each past release as well as
 the version control of each dependency.
 
 
-21.1.0 (unreleased)
+21.1.0 (2020-09-23)
 ===================
 
 kinto-changes
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name='kinto-dist',
-    version='21.1.0.dev0',
+    version='21.1.0',
     description='Kinto Distribution',
     long_description=README + "\n\n" + CHANGELOG,
     license='Apache License (2.0)',
