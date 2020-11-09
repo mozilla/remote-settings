4210913360bcb9c70ba924dcaadb8a61a18972c8
Mathieu Leplatre <mathieu@mozilla.com>
2020-11-09 22:23:08 +0100

    Update CHANGELOG
    Preparing release 21.2.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name='kinto-dist',
-    version='21.3.0.dev0',
+    version='21.2.2',
     description='Kinto Distribution',
     long_description=README + "\n\n" + CHANGELOG,
     license='Apache License (2.0)',
