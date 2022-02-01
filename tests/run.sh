1841eda40c55f8ccb3c919cbf431de5c453c8245
Mathieu Leplatre <mathieu@mozilla.com>
2022-02-01 04:29:07 +0100

    Do not build tests container from server one

--- tests/run.sh
+++ tests/run.sh
@@ -4,9 +4,9 @@ set -eo pipefail
 : "${SERVER:=http://web:8888/v1}"
 
 usage() {
-    echo "usage: ./run.sh start"
+    echo "usage: ./run.sh tests"
     echo ""
-    echo "    start                         Start tests"
+    echo "    tests                         Start tests"
     echo ""
     exit 1
 }
@@ -14,7 +14,7 @@ usage() {
 [ $# -lt 1 ] && usage
 
 case $1 in
-start)
+tests)
     wget -q --tries=180 --retry-connrefused --waitretry=1 -O /dev/null $SERVER || (echo "Can't reach $SERVER" && exit 1)
     http -q --check-status $SERVER/__heartbeat__
     pytest integration_test.py --server $SERVER
