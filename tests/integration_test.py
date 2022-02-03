3994445277fc3467160dfa74b6a4fc3a3cfb7bbf
Mathieu Leplatre <mathieu@mozilla.com>
2022-02-03 13:38:53 +0100

    Test editors and reviewers permissions on remote server

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -61,6 +61,39 @@ def test_heartbeat(server: str):
     resp.raise_for_status()
 
 
+async def test_permissions_endpoint(
+    make_client: ClientFactory,
+    server: str,
+    setup_auth: Auth,
+    editor_auth: Auth,
+    reviewer_auth: Auth,
+    source_bucket: str,
+    source_collection: str,
+    skip_server_setup: bool,
+):
+    if not skip_server_setup:
+        setup_client = make_client(setup_auth)
+        editor_client = make_client(editor_auth)
+        reviewer_client = make_client(reviewer_auth)
+        await setup_server(setup_client, editor_client, reviewer_client)
+
+    for user in (editor_auth, reviewer_auth):
+        resp = requests.get(
+            f"{server}/permissions",
+            auth=user,
+        )
+        resp.raise_for_status()
+        permissions = resp.json()["data"]
+        collection_perms = [
+            e["permissions"]
+            for e in permissions
+            if e["resource_name"] == "collection"
+            and e["bucket_id"] == source_bucket
+            and e["collection_id"] == source_collection
+        ][0]
+        assert "write" in collection_perms
+
+
 async def test_history_plugin(
     make_client: ClientFactory,
     setup_auth: Auth,
