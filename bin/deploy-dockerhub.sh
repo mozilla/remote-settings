add282cbd0c81c5d59b2efb987b4feb0f9f20b59
Graham Beckley <gbeckley@mozilla.com>
2022-05-06 09:21:13 -0400

    Edit Circle config to push integration test container

--- bin/deploy-dockerhub.sh
+++ bin/deploy-dockerhub.sh
@@ -26,13 +26,27 @@ echo "$DOCKER_PASS" | docker login -u="$DOCKER_USER" --password-stdin
 
 docker images
 
+SERVER_CONTAINER="remotesettings/server"
+TEST_CONTAINER="remotesettings/tests"
+SERVER_DOCKER_REPO="mozilla/remote-settings"
+INTEGRATION_TEST_DOCKER_REPO="mozilla/remote-settings-integration-tests"
+
 # docker tag and push git branch to dockerhub
 if [ -n "$1" ]; then
     TAG="$1"
-    echo "Tag and push ${DOCKERHUB_REPO}:${TAG} to Dockerhub"
-    docker tag remotesettings:server "$DOCKERHUB_REPO:$TAG" ||
-        (echo "Couldn't tag remote-settings as $DOCKERHUB_REPO:$TAG" && false)
-    retry 3 docker push "$DOCKERHUB_REPO:$TAG" ||
-        (echo "Couldn't push $DOCKERHUB_REPO:$TAG" && false)
+    echo "Tag and push server and integration test containers to Dockerhub"
+    echo "${SERVER_DOCKER_REPO}:${TAG}"
+    echo "${INTEGRATION_TEST_DOCKER_REPO}:${TAG}"
+
+    docker tag $SERVER_CONTAINER "$SERVER_DOCKER_REPO:$TAG" ||
+        (echo "Couldn't tag $SERVER_CONTAINER as $SERVER_DOCKER_REPO:$TAG" && false)
+    retry 3 docker push "$SERVER_DOCKER_REPO:$TAG" ||
+        (echo "Couldn't push $SERVER_DOCKER_REPO:$TAG" && false)
+
+    docker tag $TEST_CONTAINER "$INTEGRATION_TEST_DOCKER_REPO:$TAG" ||
+        (echo "Couldn't tag $TEST_CONTAINER as $INTEGRATION_TEST_DOCKER_REPO:$TAG" && false)
+    retry 3 docker push "$INTEGRATION_TEST_DOCKER_REPO:$TAG" ||
+        (echo "Couldn't push $INTEGRATION_TEST_DOCKER_REPO:$TAG" && false)
+
     echo "Done."
 fi
