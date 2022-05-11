e1addcabb20e66b10b88b44570fe1e7cd8f845a7
grahamalama <gbeckley@mozilla.com>
2022-05-11 10:51:35 -0400

    Push integration test container to Dockerhub (#221)
    * Use repository namespace for server and test containers
    
    Change server and test containers from tags to separate containers
    (`server`, `tests`) under the 'remotesettings" repository (only used
    locally).
    
    * Edit Circle config to push integration test container
    
    * Add bash function for pushing container to repo

--- bin/deploy-dockerhub.sh
+++ bin/deploy-dockerhub.sh
@@ -21,18 +21,38 @@ function retry() {
     return 0
 }
 
+# Usage: push_container_to_repo CONTAINER REPO TAG
+# Push container to specified repo with tag
+# Example: push_container_to_repo remotesettings mozilla/remote-settings latest
+function push_container_to_repo() {
+    container=$1
+    repo=$2
+    tag=$3
+    docker tag $container "$repo:$tag" ||
+        (echo "Couldn't tag $container as $repo:$tag" && false)
+    retry 3 docker push "$repo:$tag" ||
+        (echo "Couldn't push $repo:$tag" && false)
+    return 0
+}
+
 # configure docker creds
 echo "$DOCKER_PASS" | docker login -u="$DOCKER_USER" --password-stdin
 
 docker images
 
+SERVER_CONTAINER="remotesettings/server"
+TEST_CONTAINER="remotesettings/tests"
+SERVER_DOCKER_REPO="mozilla/remote-settings"
+INTEGRATION_TEST_DOCKER_REPO="mozilla/remote-settings-integration-tests"
+
 # docker tag and push git branch to dockerhub
 if [ -n "$1" ]; then
     TAG="$1"
-    echo "Tag and push ${DOCKERHUB_REPO}:${TAG} to Dockerhub"
-    docker tag remotesettings:server "$DOCKERHUB_REPO:$TAG" ||
-        (echo "Couldn't tag remote-settings as $DOCKERHUB_REPO:$TAG" && false)
-    retry 3 docker push "$DOCKERHUB_REPO:$TAG" ||
-        (echo "Couldn't push $DOCKERHUB_REPO:$TAG" && false)
+    echo "Tag and push server and integration test containers to Dockerhub"
+    echo "${SERVER_DOCKER_REPO}:${TAG}"
+    echo "${INTEGRATION_TEST_DOCKER_REPO}:${TAG}"
+    push_container_to_repo $SERVER_CONTAINER $SERVER_DOCKER_REPO $TAG
+    push_container_to_repo $TEST_CONTAINER $INTEGRATION_TEST_DOCKER_REPO $TAG
+
     echo "Done."
 fi
