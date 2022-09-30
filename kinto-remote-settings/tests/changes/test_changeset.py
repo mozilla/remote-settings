ceaa4cf58eded1c11e055fbf1847c98a82b991c0
Mathieu Leplatre <mathieu@mozilla.com>
2022-09-30 18:23:23 +0200

    Add cache-control headers on all changeset endpoints

--- kinto-remote-settings/tests/changes/test_changeset.py
+++ kinto-remote-settings/tests/changes/test_changeset.py
@@ -24,6 +24,7 @@ class ChangesetViewTest(BaseWebTest, unittest.TestCase):
     @classmethod
     def get_app_settings(cls, extras=None):
         settings = super().get_app_settings(extras)
+        settings["record_cache_expires_seconds"] = "180"
         settings["blocklists.certificates.record_cache_expires_seconds"] = 1234
         return settings
 
@@ -128,6 +129,16 @@ class ChangesetViewTest(BaseWebTest, unittest.TestCase):
     def test_cache_control_headers_are_set(self):
         resp = self.app.get(self.changeset_uri, headers=self.headers)
         assert resp.headers["Cache-Control"] == "max-age=1234"
+
+    def test_cache_control_has_default(self):
+        self.create_collection("blocklists", "addons")
+
+        resp = self.app.get(
+            "/buckets/blocklists/collections/addons/changeset?_expected=0",
+            headers=self.headers,
+        )
+
+        assert resp.headers["Cache-Control"] == "max-age=180"
 
     def test_raises_original_backend_errors(self):
         backend = self.app.app.registry.storage
