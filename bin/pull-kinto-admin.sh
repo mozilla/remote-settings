5334abe9705540a066ae4893c58e0b427ddc25b0
Mathieu Leplatre <mathieu@mozilla.com>
2024-02-01 10:25:19 +0100

    Upgrade kinto-admin to 3.0.3 (#540)
    * Upgrade kinto-admin to 3.0.3
    
    * Pulling latest kinto-admin single-server build rather than building it again
    
    * forgot to make the folder
    
    ---------
    
    Co-authored-by: Alex Cottner <acottner@mozilla.com>

---
+++ bin/pull-kinto-admin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+VERSION=$(cat `pwd`/kinto-admin/VERSION)
+TAG="v${VERSION}"
+
+# download and unzip release
+curl -OL https://github.com/Kinto/kinto-admin/releases/download/${TAG}/kinto-admin-release.tar
+mkdir ./kinto-admin/build
+tar -xf kinto-admin-release.tar -C ./kinto-admin/build && rm kinto-admin-release.tar
