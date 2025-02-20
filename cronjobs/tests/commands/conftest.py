e81871b3a251e3f0368ee9b464becd82eeeb46e6
Mathieu Leplatre <mathieu@mozilla.com>
2025-02-20 12:25:59 +0100

    Add missing conftest

---
+++ cronjobs/tests/commands/conftest.py
@@ -0,0 +1,9 @@
+import sys
+from pathlib import Path
+
+
+# Commands are not a Python package, just files in container.
+# Add src/ to Python path.
+src_path = (Path(__file__).resolve().parent.parent.parent / "src")
+
+sys.path.insert(0, str(src_path))
