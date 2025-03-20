49c1eea1f46887b50bc1c00705719b358e67ce6e
Mathieu Leplatre <mathieu@mozilla.com>
2025-03-20 10:58:42 +0100

    Fix #805: prevent crash when since value contains trailing new line

--- kinto-remote-settings/src/kinto_remote_settings/changes/views.py
+++ kinto-remote-settings/src/kinto_remote_settings/changes/views.py
@@ -257,7 +257,7 @@ class QuotedTimestamp(colander.SchemaNode):
 
     schema_type = colander.String
     error_message = "The value should be integer between double quotes."
-    validator = colander.Regex('^"([0-9]+?)"$', msg=error_message)
+    validator = colander.Regex('^"([0-9]+?)"(?!\n)$', msg=error_message)
 
     def deserialize(self, cstruct=colander.null):
         param = super(QuotedTimestamp, self).deserialize(cstruct)
