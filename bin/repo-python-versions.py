2e12e348f5d691dbb07607512279b9ab756985dc
Mathieu Leplatre <mathieu@mozilla.com>
2025-03-21 13:55:43 +0100

    Only verify that CI runs the same version as produced Docker image

--- bin/repo-python-versions.py
+++ bin/repo-python-versions.py
@@ -6,31 +6,6 @@ from pathlib import Path
 REGEXP_VERSION = r"[0-9]+\.[0-9]+(?:\.[0-9]+)?"
 
 
-def extract_version_from_readthedocs(file_path):
-    """
-    .readthedocs.yaml:    python: "3.11"
-    """
-    content = file_path.read_text()
-    match = re.search(r'python:\s*"?(' + REGEXP_VERSION + ')"?', content)
-    return match.group(1) if match else None
-
-
-def extract_version_from_pyproject(file_path):
-    """ "
-    pyproject.toml:python = ">=3.11, <3.14"
-    cronjobs/pyproject.toml:python = ">=3.11, <3.14"
-    browser-tests/pyproject.toml:python = ">=3.11, <3.14""
-    """
-    content = file_path.read_text()
-    match = re.search(r'python\s*=\s*"(.*?)"', content)
-    if match:
-        version_range = match.group(1)
-        min_version_match = re.search(r">=\s*(" + REGEXP_VERSION + ")", version_range)
-        if min_version_match:
-            return min_version_match.group(1)
-    return None
-
-
 def extract_versions_from_dockerfile(file_path):
     """
     cronjobs/Dockerfile:FROM python:3.13.1 AS build
@@ -48,9 +23,7 @@ def extract_versions_from_ci_workflow(file_path):
 
 
 def main():
-    versions = [extract_version_from_readthedocs(Path(".readthedocs.yaml"))]
-    for path in Path(".").rglob("pyproject.toml"):
-        versions += [extract_version_from_pyproject(path)]
+    versions = []
     for path in Path(".github/workflows").glob("*.y*ml"):
         versions += extract_versions_from_ci_workflow(path)
     for path in Path(".").rglob("Dockerfile"):
