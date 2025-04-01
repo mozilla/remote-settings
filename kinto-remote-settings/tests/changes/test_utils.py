f923084babf0abfb217478484ff36be8a781e107
Mathieu Leplatre <mathieu@mozilla.com>
2025-04-01 17:48:10 +0200

    Fix #830: remove query in a loop using new storage method of kinto 20.2.0 (#837)
    * Fix #830: remove query in a loop using new storage method of kinto 20.2.0
    * Run poetry lock

--- kinto-remote-settings/tests/changes/test_utils.py
+++ kinto-remote-settings/tests/changes/test_utils.py
@@ -1,41 +1,25 @@
 import unittest
 from unittest import mock
 
-from kinto_remote_settings.changes.utils import changes_object
+from kinto_remote_settings.changes.utils import change_entry_id
 from pyramid.request import Request
 
 
-class ChangesRecordTest(unittest.TestCase):
+class ChangesEntryIdTest(unittest.TestCase):
     def test_single_hardcoded(self):
         request = Request.blank(path="/")
         request.route_path = mock.Mock()
         request.route_path.return_value = "/buckets/a/collections/b"
-        request.registry = mock.Mock()
-        request.registry.settings = {}
-        timestamp = 1525457597166
-        entry = changes_object(request, "a", "b", timestamp)
 
-        assert entry == {
-            "bucket": "a",
-            "collection": "b",
-            "host": "",
-            "id": "9527d115-6191-fa49-a530-8fbfc4997755",
-            "last_modified": timestamp,
-        }
+        entry = change_entry_id(request, "", "a", "b")
+
+        assert entry == "9527d115-6191-fa49-a530-8fbfc4997755"
 
     def test_another_hardcoded(self):
         request = Request.blank(path="/")
         request.route_path = mock.Mock()
         request.route_path.return_value = "/buckets/a/collections/b"
-        request.registry = mock.Mock()
-        request.registry.settings = {"http_host": "https://localhost:443"}
-        timestamp = 1525457597166
-        entry = changes_object(request, "a", "b", timestamp)
 
-        assert entry == {
-            "bucket": "a",
-            "collection": "b",
-            "host": "https://localhost:443",
-            "id": "fa48a96d-1600-f561-8645-3395acb08a5a",
-            "last_modified": timestamp,
-        }
+        entry = change_entry_id(request, "https://localhost:443", "a", "b")
+
+        assert entry == "fa48a96d-1600-f561-8645-3395acb08a5a"
