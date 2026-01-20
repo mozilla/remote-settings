cd7cc1b713150cf84e5843a73d81f6b780b50b26
Mathieu Leplatre <mathieu@mozilla.com>
2026-01-20 16:49:02 +0100

    Fix display of referenced attachments (#1138)
    * Fix display of
    referenced attachments
    
    * Rewrite objects completely, from scratch
    
    * Simplify slightly

--- cronjobs/src/commands/git_export.py
+++ cronjobs/src/commands/git_export.py
@@ -499,7 +499,9 @@ def process_attachments(
         if existing := existing_attachments.get(location):
             existing_hash, existing_size = existing
         if existing_hash != hash or existing_size != size:
-            print(f"Bundle {path} {'is new' if existing_hash is None else 'has changed'}")
+            print(
+                f"Bundle {path} {'is new' if existing_hash is None else 'has changed'}"
+            )
             changed_attachments.append((hash, size, url))
     return changed_attachments, common_content
 
