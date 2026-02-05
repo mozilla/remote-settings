e76075b928c6a12792e41eedc950ab4fc206b08c
Mathieu Leplatre <mathieu@mozilla.com>
2026-02-05 17:43:37 +0100

    Rewrite debounced broadcasts view (#1160)
    * Rewrite debounce of broadcast endpoint
    
    * Adjust tests
    
    * @alexcottner review
    
    * Skip preview collections
    
    * Simplify everything by removing max_debounce

--- kinto-remote-settings/src/kinto_remote_settings/changes/views.py
+++ kinto-remote-settings/src/kinto_remote_settings/changes/views.py
@@ -522,62 +522,69 @@ def broadcasts_view(request):
             "push_broadcast_min_debounce_interval_seconds", "300"
         )  # 5 min by default.
     )
-    max_debounce_interval = int(
-        settings.get(
-            "push_broadcast_max_debounce_interval_seconds", "1200"
-        )  # 20 min by default.
-    )
 
-    # Current highest timestamp in the monitored collections:
-    rs_timestamp = max(ts for _, _, ts in monitored_timestamps(request))
+    def get_rs_timestamp():
+        # We want to filter out preview entries, because we don't want to notify all clients
+        # when a review is requested.
+        # Note: This will also filter out the `nimbus-preview` collection which isn't directly
+        # consumed by clients and therefore doesn't need to trigger a broadcast when it changes.
+        return max(
+            ts
+            for bid, cid, ts in monitored_timestamps(request)
+            if "-preview" not in f"{bid}/{cid}"
+        )
 
-    # Last published timestamp (from cache).
+    # First, get the current value from cache.
     cache_key = f"{BROADCASTER_ID}/{CHANNEL_ID}/timestamp"
-    last_timestamp = request.registry.cache.get(cache_key)
-    if last_timestamp is None:
+    last_published_timestamp = request.registry.cache.get(cache_key)
+    debounced_timestamp = last_published_timestamp
+
+    if last_published_timestamp is None:
         # If no timestamp was published ever, we use the current timestamp.
-        debounced_timestamp = rs_timestamp
+        debounced_timestamp = get_rs_timestamp()
         logger.info(
             "No previous timestamp found in cache. Publishing current timestamp."
         )
     else:
-        # Avoid publishing too many Push notifications in a short time:
-        # - if changes are published too close together (eg. < 5min), then we don't update the exposed timestamp
-        # - but if changes are published continuously for too long (eg. > 20min), then we update the exposed timestamp
-
+        # If the last published timestamp is recent, we don't publish a new one yet.
         last_timestamp_age_seconds = (
-            utcnow() - datetime.fromtimestamp(last_timestamp / 1000, timezone.utc)
+            utcnow()
+            - datetime.fromtimestamp(last_published_timestamp / 1000, timezone.utc)
         ).total_seconds()
 
-        # Have we published a change recently?
-        diff = min_debounce_interval - last_timestamp_age_seconds
-        if diff > 0:
-            log_msg = f"A change was published recently (<{min_debounce_interval}). "
-            if last_timestamp_age_seconds < max_debounce_interval:
-                log_msg += f"Last timestamp is {last_timestamp_age_seconds} seconds old (<{max_debounce_interval}). Can wait {diff} more seconds."
-                # Do not publish a new timestamp yet.
-                debounced_timestamp = last_timestamp
+        # Avoid publishing too many Push notifications in a short time:
+        # if changes are published too close together (eg. < 5min), then
+        # we don't update the exposed timestamp
+        if last_timestamp_age_seconds > min_debounce_interval:
+            # Let's see if a new change is available.
+            current_rs_timestamp = get_rs_timestamp()
+            if current_rs_timestamp > last_published_timestamp:
+                log_msg = "A new change is available. "
+                # Check if it's too close to the last published change.
+                current_timestamp_diff_seconds = (
+                    current_rs_timestamp - last_published_timestamp
+                ) / 1000
+                # We publish only if more than `min_debounce_interval` seconds have passed since the last published change.
+                if current_timestamp_diff_seconds > min_debounce_interval:
+                    log_msg += "Publish!"
+                    debounced_timestamp = current_rs_timestamp
+                else:
+                    log_msg += f"It's too close to previously broadcasted ({current_timestamp_diff_seconds}<={min_debounce_interval})."
             else:
-                log_msg += f"Last timestamp is {last_timestamp_age_seconds} seconds old (>{max_debounce_interval}). Publish!"
-                # Publish a new timestamp.
-                debounced_timestamp = rs_timestamp
+                log_msg = "Nothing to do. No new change since last published timestamp."
         else:
-            # No, we haven't, publish a new timestamp!
-            log_msg = f"Last timestamp is {last_timestamp_age_seconds} seconds old (>{min_debounce_interval}). Publish!"
-            debounced_timestamp = rs_timestamp
+            log_msg = f"Nothing to do. A change was published very recently ({last_timestamp_age_seconds}<={min_debounce_interval})."
 
         logger.info(
             log_msg,
             extra={
                 "min_debounce_interval": min_debounce_interval,
-                "max_debounce_interval": max_debounce_interval,
                 "last_timestamp_age_seconds": last_timestamp_age_seconds,
-                "wait_seconds": max(0, diff),
             },
         )
 
     # Store the published timestamp in the cache for next calls (skip write if unchanged).
-    if debounced_timestamp != last_timestamp:
+    if debounced_timestamp != last_published_timestamp:
         request.registry.cache.set(cache_key, debounced_timestamp, ttl=DAY_IN_SECONDS)
     # Expose it for the Push service to pull.
     return {
--- kinto-remote-settings/tests/changes/test_broadcasts.py
+++ kinto-remote-settings/tests/changes/test_broadcasts.py
@@ -7,6 +7,7 @@ from . import BaseWebTest
 
 FAKE_NOW = datetime.datetime(2023, 10, 1, 12, 0, 0, tzinfo=datetime.timezone.utc)
 DAY_IN_SECONDS = 24 * 60 * 60
+FAKE_TIMESTAMP = int(FAKE_NOW.timestamp() * 1000)
 
 
 class BroadcastsTest(BaseWebTest, unittest.TestCase):
@@ -25,72 +26,176 @@ class BroadcastsTest(BaseWebTest, unittest.TestCase):
         self.monitored_timestamps = monitored_timestamps.start()
         self.addCleanup(monitored_timestamps.stop)
 
+    def tearDown(self):
+        super().tearDown()
+        self.app.app.registry.cache.flush()
+
     def get_broadcasted_version(self):
         response = self.app.get("/__broadcasts__")
         return response.json["broadcasts"]["remote-settings/monitor_changes"]
 
     def test_first_call_returns_current_version(self):
-        self.monitored_timestamps.return_value = [("main", "cid", 42)]
+        self.monitored_timestamps.return_value = [
+            ("main", "cid", FAKE_TIMESTAMP),
+            ("main", "cid2", FAKE_TIMESTAMP - 1000),
+        ]
         self.app.app.registry.cache.flush()
 
-        assert self.get_broadcasted_version() == '"42"'
+        assert self.get_broadcasted_version() == f'"{FAKE_TIMESTAMP}"'
+
+    def test_preview_buckets_and_collections_are_ignored(self):
+        self.monitored_timestamps.return_value = [
+            ("main-preview", "cid", FAKE_TIMESTAMP + 2000),
+            ("main", "cid-preview", FAKE_TIMESTAMP + 1000),
+            ("main", "cid", FAKE_TIMESTAMP),
+        ]
+        self.app.app.registry.cache.flush()
+
+        assert self.get_broadcasted_version() == f'"{FAKE_TIMESTAMP}"'
 
     def test_returns_current_value_if_up_to_date(self):
-        self.monitored_timestamps.return_value = [("main", "cid", 42)]
+        self.monitored_timestamps.return_value = [("main", "cid", FAKE_TIMESTAMP)]
 
-        assert self.get_broadcasted_version() == '"42"'
-        assert self.get_broadcasted_version() == '"42"'
+        assert self.get_broadcasted_version() == f'"{FAKE_TIMESTAMP}"'
+        assert self.get_broadcasted_version() == f'"{FAKE_TIMESTAMP}"'
+        self.monitored_timestamps.assert_called_once()  # Only one call to the database, second call is cached
 
-    def test_return_current_version_if_last_publish_is_older_than_max_debounce(self):
-        # Last timestamp is too old (> max debounce), new one should be published
+    def test_database_is_not_hit_when_last_published_is_younger_than_min_debounce(self):
+        """
+        ================o=>>>
+                        |
+                        +----> latest published (3 min ago < min debounce)
+        """
         old_timestamp = int(
-            (FAKE_NOW - datetime.timedelta(minutes=25)).timestamp() * 1000
+            (FAKE_NOW - datetime.timedelta(minutes=3)).timestamp() * 1000
         )
         self.app.app.registry.cache.set(
             "remote-settings/monitor_changes/timestamp",
             old_timestamp,
             ttl=DAY_IN_SECONDS,
         )
-        new_timestamp = int(
-            (FAKE_NOW - datetime.timedelta(minutes=3)).timestamp() * 1000
-        )
-        self.monitored_timestamps.return_value = [("main", "cid", new_timestamp)]
 
-        assert self.get_broadcasted_version() == f'"{new_timestamp}"'
+        assert self.get_broadcasted_version() == f'"{old_timestamp}"'
+        self.monitored_timestamps.assert_not_called()
 
-    def test_return_last_version_if_last_publish_is_younger_than_max_debounce(
+    def test_ignores_intermediary_versions_aka_debounce(self):
+        """
+        ===o==x==x==x==o==========>>>
+           |  |  |  |  |
+           |  |  |  |  +-------------> latest timestamp published
+           |  |  |  |
+           |  +--+--+----------------> intermediary timestamps are ignored (aka. debounce)
+           |
+           +-------------------------> latest published
+        """
+        obtained = []
+        for age in range(12, 5, -1):
+            # Go back in time for each iteration to simulate the passage of time
+            # and the publication of intermediary changes.
+            self.utcnow.return_value = FAKE_NOW - datetime.timedelta(minutes=age)
+            # One change every minute.
+            latest_timestamp = int(
+                (FAKE_NOW - datetime.timedelta(minutes=age + 1)).timestamp() * 1000
+            )
+            self.monitored_timestamps.return_value = [("main", "cid", latest_timestamp)]
+            obtained.append(self.get_broadcasted_version())
+
+        assert obtained == [obtained[0]] * 6 + [obtained[-1]]
+
+    def test_return_cache_if_cached_and_current_are_older_than_min_debounce(
         self,
     ):
-        # New timestamp is too fresh (< debounce), and last timestamp is still valid
+        """
+        ===ox========>>>
+           ||
+           |+--> latest timestamp (6 min ago, but no change since then), return cache!
+           |
+           +-------------------> latest published
+        """
         latest_timestamp = int(
-            (FAKE_NOW - datetime.timedelta(minutes=42)).timestamp() * 1000
+            (FAKE_NOW - datetime.timedelta(minutes=6)).timestamp() * 1000
+        )
+        cache_timestamp = latest_timestamp
+        self.app.app.registry.cache.set(
+            "remote-settings/monitor_changes/timestamp",
+            cache_timestamp,
+            ttl=DAY_IN_SECONDS,
         )
         self.monitored_timestamps.return_value = [("main", "cid", latest_timestamp)]
-        last_published = int(
-            (FAKE_NOW - datetime.timedelta(minutes=3)).timestamp() * 1000
-        )
-        self.app.app.registry.cache.set(
-            "remote-settings/monitor_changes/timestamp",
-            last_published,
-            ttl=DAY_IN_SECONDS,
-        )
 
-        assert self.get_broadcasted_version() == f'"{last_published}"'
+        assert self.get_broadcasted_version() == f'"{cache_timestamp}"'
 
-    def test_return_current_version_if_last_publish_exceeds_max_debounce(
+    def test_return_cache_version_if_latest_changes_are_newer_than_min_debounce(
         self,
     ):
-        old_timestamp = int(
-            (FAKE_NOW - datetime.timedelta(minutes=21)).timestamp() * 1000
+        """
+        ======o==x==>>>
+              |  |
+              |  +----> latest timestamp (2min ago but only 4 min diff with previous), return cache!
+              |
+              +--------> latest published (6min ago, > min )
+        """
+        latest_timestamp = int(
+            (FAKE_NOW - datetime.timedelta(minutes=2)).timestamp() * 1000
+        )
+        cache_timestamp = int(
+            (FAKE_NOW - datetime.timedelta(minutes=6)).timestamp() * 1000
         )
         self.app.app.registry.cache.set(
             "remote-settings/monitor_changes/timestamp",
-            old_timestamp,
+            cache_timestamp,
             ttl=DAY_IN_SECONDS,
         )
-        recent_timestamp = int(
-            (FAKE_NOW - datetime.timedelta(minutes=3)).timestamp() * 1000
-        )
-        self.monitored_timestamps.return_value = [("main", "cid", recent_timestamp)]
+        self.monitored_timestamps.return_value = [("main", "cid", latest_timestamp)]
 
-        assert self.get_broadcasted_version() == f'"{recent_timestamp}"'
+        assert self.get_broadcasted_version() == f'"{cache_timestamp}"'
+
+    def test_return_cache_version_if_latest_changes_are_older_than_min_debounce(
+        self,
+    ):
+        """
+        ===o======x=>>>
+           |      |
+           |      +--> latest timestamp (2min ago but 6 min diff with previous > min debounce), publish!
+           |
+           +---------> latest published (8 min ago > min debounce)
+        """
+        latest_timestamp = int(
+            (FAKE_NOW - datetime.timedelta(minutes=2)).timestamp() * 1000
+        )
+        cache_timestamp = int(
+            (FAKE_NOW - datetime.timedelta(minutes=8)).timestamp() * 1000
+        )
+        self.app.app.registry.cache.set(
+            "remote-settings/monitor_changes/timestamp",
+            cache_timestamp,
+            ttl=DAY_IN_SECONDS,
+        )
+        self.monitored_timestamps.return_value = [("main", "cid", latest_timestamp)]
+
+        assert self.get_broadcasted_version() == f'"{latest_timestamp}"'
+
+    def test_return_current_version_if_latest_changes_are_older_than_min_debounce(
+        self,
+    ):
+        """
+        ==o==x======>>>
+          |  |
+          |  +----> latest timestamp (6 min ago, but 2 min diff with previous, too close)
+          |
+          +-------> latest published (8 min ago > min debounce)
+        """
+        latest_timestamp = int(
+            (FAKE_NOW - datetime.timedelta(minutes=6)).timestamp() * 1000
+        )
+        cache_timestamp = int(
+            (FAKE_NOW - datetime.timedelta(minutes=8)).timestamp() * 1000
+        )
+        self.app.app.registry.cache.set(
+            "remote-settings/monitor_changes/timestamp",
+            cache_timestamp,
+            ttl=DAY_IN_SECONDS,
+        )
+        self.monitored_timestamps.return_value = [("main", "cid", latest_timestamp)]
+
+        assert self.get_broadcasted_version() == f'"{cache_timestamp}"'
