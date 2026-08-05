3ee323ad88d50514a55f382e0dfdd71f85f7e458
Mathieu Leplatre <mathieu@mozilla.com>
2026-08-05 17:57:34 +0200

    git-reader: run `git clean` in `gitupdate` to remove untracked files (#1453)
    * git-reader: run git clean in gitupdate
    
    * Show LFS version

--- git-reader/run.sh
+++ git-reader/run.sh
@@ -197,10 +197,12 @@ git_fetch_lfs() {
     git -C "$repo_path" reset --hard "$ORIGIN_NAME/v1/common"
     log "Cleaning up repository..."
     git -C "$repo_path" remote prune "$ORIGIN_NAME"
+    git -C "$repo_path" clean -f -d
     git -C "$repo_path" gc --prune=now
     git -C "$repo_path" repack -Ad
 
     if [ "${SELF_CONTAINED:-false}" = "true" ]; then
+        git -C "$repo_path" lfs version
         log "Fetching LFS objects..."
         git -C "$repo_path" lfs pull
         log "Pruning LFS objects..."
